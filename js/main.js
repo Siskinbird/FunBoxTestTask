0fc08acaa67ce95947d5c33f3ebb27f2e5b00e18
Dmitrii Chizhov <dimachig@gmail.com>
2022-06-23 17:40:43 +0300

    fix: cards checked problem solution

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
+'use strict'
+
 let catCard = document.querySelectorAll('.cat-items__item');
-let titleSpan = document.querySelectorAll('.title-span');
 let buy = document.querySelectorAll('.buy');
+let catBlock = document.querySelector('.cat-items')
+let titleSpan = document.querySelectorAll('.title-span');
 
+catCard.forEach((item) => {
+    item.addEventListener('click', select)
+})
 
+/* catCard.forEach((item) => {
+    item.addEventListener('mouseenter', changeTitle)
+})
 
-for(let i = 0; i < catCard.length; i++) {
-    catCard[i].addEventListener('click', select)
-}
+catCard.forEach((item) => {
+    item.addEventListener('mouseleave', addTitle)
+}) */
 
-/* catCard.addEventListener('mouseout', changeTitle); */
 
 function select(e) {
     if(e.currentTarget.classList.contains('select')){
         e.currentTarget.classList.remove('select');
-        selectBuy()
+        console.log(e.target)
     } else {
         e.currentTarget.classList.add('select'); 
+        console.log(e.target)
     }
 }
 
-function show(event) {
-    console.log(event.type);
-    console.log(event.target);
-}
 
+catCard.forEach((card) => card.addEventListener('mouseenter', (e) => {
+    const element = e.target.closest('.cat-items__item');
+    const elementIndex = Array.prototype.slice.call(catCard).indexOf(element);
+    if(element.classList.contains('select')) {
+        titleSpan[elementIndex].textContent = 'Котэ не одобряет?';
+        titleSpan[elementIndex].style.color =  '#D91667';
+    } else {
+        titleSpan[elementIndex].textContent = 'Сказочное заморское яство';
+        titleSpan[elementIndex].style.color =  '#666666';
+    }   
+}))
+
+catCard.forEach((card) => card.addEventListener('mouseleave', (e) => {
+    const element = e.target.closest('.cat-items__item');
+    const elementIndex = Array.prototype.slice.call(catCard).indexOf(element);
+    titleSpan[elementIndex].textContent = 'Сказочное заморское яство';
+    titleSpan[elementIndex].style.color =  '#666666';
+}))
